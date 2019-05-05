2a078eedd195660832bf597fc48af201a556602a
JohnHarlepas <harlepas@gmail.com>
2019-05-05 15:06:05 -0400

    added structure and pics

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="textBack">
+        <h1>Dating Game!</h1>
+        <h2>Click on an date to earn points, but don't pick the same date twice or you lose!</h2>
+
+      </div>
+      
+    </header>
+  );
+}
+
+export default Header;
